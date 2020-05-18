ea5419506449186a5d1d86b73cc539f85ae1bc2c
Salim Fadhley <salimfadhley@gmail.com>
2020-05-18 14:04:06 +0100

    Solved meetup

--- scala/meetup/src/main/scala/Meetup.scala
+++ scala/meetup/src/main/scala/Meetup.scala
@@ -4,7 +4,20 @@ import Schedule.Schedule
 
 case class Meetup(month: Int, year: Int) {
 
-  def day(dayOfWeek: Int, schedule: Schedule): LocalDate = ???
+  def day(dayOfWeek: Int, schedule: Schedule): LocalDate = {
+    val firstDay = LocalDate.of(year, month, 1)
+    val candidate_days = (0 to 32).map(firstDay.plusDays(_)).filter(_.getMonthValue == month).filter(dayOfWeek == _.getDayOfWeek.getValue)
+
+    schedule match {
+      case Schedule.Teenth => candidate_days.filter((date: LocalDate) => 13 <= date.getDayOfMonth && date.getDayOfMonth <= 19).head
+      case Schedule.First => candidate_days.head
+      case Schedule.Second => candidate_days(1)
+      case Schedule.Third => candidate_days(2)
+      case Schedule.Fourth => candidate_days(3)
+      case Schedule.Last => candidate_days.last
+    }
+
+  }
 }
 
 object Schedule extends Enumeration {
