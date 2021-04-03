1a40b391ccd950fa35c52ac0a2351a1868bec81a
Salim Fadhley <salimfadhley@gmail.com>
2021-04-03 23:58:53 +0100

    Add simple linked list

---
+++ scala/simple-linked-list/src/main/scala/SimpleLinkedList.scala
@@ -0,0 +1,11 @@
+trait SimpleLinkedList[T] {
+  def isEmpty: Boolean
+  def value: T
+  def add(item: T): SimpleLinkedList[T]
+  def next: SimpleLinkedList[T]
+  def reverse: SimpleLinkedList[T]
+  def toSeq: Seq[T]
+}
+
+
+class SimpleLinkedLst[T] extends SimpleLinkedList
